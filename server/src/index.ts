58bef5f45e3bd15fc8f9384402a517cc7efbe344
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2024-05-21 10:30:15 +0100

    adding module and subject to prompt

--- server/src/index.ts
+++ server/src/index.ts
@@ -24,6 +24,8 @@ app.use(cors({
 app.get('/api/questions', async (req, res) => {
     console.log("GET /api/questions")
     const text = req.query.lesson
+    const module = req.query.module
+    const subject = req.query.subject
     console.log("text", text)
 
     const openai = new Groq({
@@ -31,7 +33,7 @@ app.get('/api/questions', async (req, res) => {
     })
     const model = "llama3-8b-8192"
 
-    const frenchPrompt = `Lisez tout le texte. Générez le maximum des questions questions possible avec 5 réponses exactement à choix multiple (c'est à dire une question peut avoir plusieurs réponses justes) à partir de ce texte : ${text} pour les étudiants en médecine, les questions doivent avoir plusieurs réponses justes et elles doient être difficie, et issues tout le text et que le texte! Fournissez également les réponses séparément (1, 2, 3, 4, 5). Et n'envoyez que le JSON directement, sans texte d'introduction s'il vous plaît, pour que le challenge réussisse ! Votre réponse doit être dans le format suivant : {"questions": [{"id": 0, "question": "", "options": [], "answers": []}, ...], answers array should contain only numbers of the correct answers}`
+    const frenchPrompt = `Lisez tout le texte avant de générer les questions. Générez 19 questions questions possible avec 5 réponses exactement à choix multiple (c'est à dire une question peut avoir plusieurs réponses justes) à partir de ce texte : ${text} pour les étudiants en médecine, le cours est ${subject}, le module est ${module}, les questions doivent avoir plusieurs réponses justes et elles doient être difficie, et issues tout le text et que le texte! Fournissez également les réponses séparément (1, 2, 3, 4, 5). Et n'envoyez que le JSON directement, sans texte d'introduction s'il vous plaît, pour que le challenge réussisse ! Votre réponse doit être dans le format suivant : {"questions": [{"id": 0, "question": "", "options": [], "answers": []}, ...], answers array should contain only numbers of the correct answers}`
     const englishPrompt = `You are a quiz master and a medical professional that wants to create a quiz containing the maximum number of questions from this text: ${text} for students. Generate as many questions as possible with 5 multiple choice answers each. Also, provide the answer separately (1, 2, 3, 4, 5). Your response should be in the following format: {"questions": [{"id": 0, "question": "", "options": [], "answer": 1}, ...]}`
     
     const completion = await openai.chat.completions.create({
