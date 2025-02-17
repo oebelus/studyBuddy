72cc3ad12460cf449d652fba33204d1f1ddb3630
oebelus <imaneelfidrissi@hotmail.com>
2025-02-17 20:29:15 +0100

    prompt modification

--- server/src/resources/quiz/controllers/generate.controller.ts
+++ server/src/resources/quiz/controllers/generate.controller.ts
@@ -38,12 +38,10 @@ class GenerateController {
     ) => {
         const { lesson, module, subject, type, language, n } = req.body
 
-        console.log(lesson, module, subject, type, language, n)
-
         const genAI = new GoogleGenerativeAI(this.GEMINI_API_KEY!);
         const model = genAI.getGenerativeModel({model: "gemini-1.0-pro"})
 
-        const frenchPromptMcq = `Générer JSON en Francais à partir d'un texte, vous êtes un ${module} professeur. les questions doivent avoir plusieurs réponses justes et elles doient être difficie, et issues tout le text et que le texte! Fournissez également les réponses séparément (1, 2, 3, 4, 5). Et n'envoyez que le JSON directement, sans texte d'introduction s'il vous plaît, pour que le challenge réussisse ! Votre réponse doit être dans le format suivant : {"questions": [{"id": 0, "question": "", "options": [], "answers": []}, ...], answers array should contain only numbers of the correct answers, Lisez tout le texte avant de générer un JSON. Générez ${n} questions possible avec 5 réponses exactement à choix multiple (c'est à dire une question peut avoir plusieurs réponses justes) à partir de ce texte : ${module} pour les étudiants en ${module} (ils ne comprennent que le Français), le cours est ${subject}, le module est ${module}, `
+        const frenchPromptMcq = `Générer JSON en Francais à partir de ce texte: ${lesson}, vous êtes un ${module} professeur. les questions doivent avoir plusieurs réponses justes (avec au moins une réponse fausse) et elles doient être difficie, et issues tout le text et que le texte! Fournissez également les réponses séparément (1, 2, 3, 4, 5). Et n'envoyez que le JSON directement, sans texte d'introduction s'il vous plaît, pour que le challenge réussisse ! Votre réponse doit être dans le format suivant : {"questions": [{"id": 0, "question": "", "options": [], "answers": []}, ...], answers array should contain only numbers of the correct answers, Lisez tout le texte avant de générer un JSON. Générez ${n} questions possible avec 5 réponses exactement à choix multiple (c'est à dire une question peut avoir plusieurs réponses justes) à partir de ce texte : ${module} pour les étudiants en ${module} (ils ne comprennent que le Français), le cours est ${subject}, le module est ${module}, `
         const frenchPromptFlashcards = `Lisez tout le texte avant de générer un JSON en Francais. Générez plus de 19 flashcards à partir de ce texte : ${lesson} pour les étudiants en ${module} (ils ne comprennent que le Français), le cours est ${subject}, le module est ${module}! Votre réponse doit être sous le format suivant : {"questions": [{"id": 0, "question": "", "answer": ""}, ...]}, où answer doit contenir un seul élément (la réponse à la question). Assurez-vous que chaque objet question/réponse est bien formaté et que le JSON est valide.`
 
         const englishPromptMcq = `Generate JSON from a text, you are a ${module} professor. The questions should have multiple correct answers and they should be difficult, and derived from the entire text and only the text! Also provide the answers separately (1, 2, 3, 4, 5). And send only the JSON directly, without introductory text please, for the challenge to succeed! Your response should be in the following format: {"questions": [{"id": 0, "question": "", "options": [], "answers": []}, ...], answers array should contain only numbers of the correct answers. Read the entire text before generating the JSON. Generate ${n} possible questions with exactly 5 multiple-choice answers (i.e., a question can have multiple correct answers) from this text: ${module} for ${module} students (they only understand French), the course is ${subject}, the module is ${module}.`   
@@ -57,8 +55,6 @@ class GenerateController {
                 : (type === "flashcard" && language === "english")
                 ? englishPromptFlashcards
                 : frenchPromptFlashcards;
-
-            console.log(language)
 
             const result = await model.generateContent(prompt);
 
